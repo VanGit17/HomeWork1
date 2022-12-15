a2a1f7f72322b972c6dde0f55e60bef50d133cb8
Van <bigfilessender@mail.ru>
2022-12-15 23:27:59 +0300

    Решена третья задача

---
+++ Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("давайте найдем максимальное из трех чисел ");
+Console.Write("введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+if (a>b)
+{
+  if (a>c)
+       { 
+        Console.Write("наибольшее число: ");
+        Console.Write(a);
+        }
+        else
+        { Console.Write("наибольшее число: ");
+        Console.Write(c);}
+}
+else
+{
+  if (b>c)
+       { 
+        Console.Write("наибольшее число: ");
+        Console.Write(b);
+        }
+        else
+        { Console.Write("наибольшее число: ");
+        Console.Write(c);}  
+}
+//else 
+//{
+    //Console.Write("большее число: ");
+//Console.WriteLine(b); 
+////Console.Write("меньшее число: ");
+//Console.WriteLine(a);  
+//}
