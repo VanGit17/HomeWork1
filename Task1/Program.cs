0c3ad5968b1f6ebddc2ae26a1a3e70bfd294e4a2
Van <bigfilessender@mail.ru>
2022-12-15 22:38:59 +0300

    создали проект решили первую задачку

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("А давайте проверим, является ли первое число квадратом второго! ");
+Console.Write("введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a==b*b)
+{
+  Console.Write("Ух ты! Первое число дейcтвительно квадрат второго! бывает же! ");
+
+}
+else 
+{
+    Console.Write("Нет, конечно нет... обычные числа. ");
+
+}
