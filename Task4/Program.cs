480b039b0dc638dad43e3c5ae85f4fc027e30b55
Van <bigfilessender@mail.ru>
2022-12-16 00:15:20 +0300

    решена задача 4

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("является ли число чётным?");
+Console.Write("введите целое число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a%2==0)
+{
+  Console.Write("Ну конечно! Это число чётное ");
+
+}
+else 
+{
+    Console.Write("Нет, число не чётное");
+
+}
