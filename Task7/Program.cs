0900e45621d5b0a27b2678d7c8a24c22b462e0cd
Van <bigfilessender@mail.ru>
2022-12-16 09:49:11 +0300

    Решена задача 7

---
+++ Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿int number = new Random().Next(10,100);
+Console.WriteLine("Мы задумали число: ");
+Console.WriteLine(number);
+
+int a = (number - number%10)/10; //определим количество десятков в числе
+int b = number - a*10; //определим количество единиц в числе
+
+if (a>b)
+{
+Console.Write("Наибольшая цифра числа: ");
+   Console.Write(a);
+}
+else  
+{
+Console.Write("Наибольшая цифра числа: ");
+   Console.Write(b);
+}
+   
