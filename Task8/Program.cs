ce8a29c083051fca1b0209d7645619cab308c9ab
Van <bigfilessender@mail.ru>
2022-12-16 09:57:34 +0300

    Решена задача 8

---
+++ Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите трехзначное целое число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (Math.Abs(number)>99&Math.Abs(number)<1000)
+{
+   int a = (number - number%100)/100; //определим сколько сотен в числе
+   int b = ((number - a*100)-(number - a*100)%10)/10; //определим сколько десятков в числе
+   int c = number - a*100 - b*10; //определим количество единиц в числе
+   
+   Console.Write("Мы удалили вторую цифру числа, получили число: ");
+   Console.WriteLine(a*10+c);
+ }
+else
+Console.WriteLine("извините, но это не трехзначное число, мы так не работаем! ");
