d4b7bec2e6ede8ac6b3ccbad639da0735e2d18cb
Van <bigfilessender@mail.ru>
2022-12-16 09:34:56 +0300

    Решена шестая задача

---
+++ Task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное целое число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (Math.Abs(number)>99&Math.Abs(number)<1000)
+{
+   int a = (number%100 - (number%100)%10)/10; 
+    Console.Write("Вторая цифра числа: ");
+   Console.WriteLine(Math.Abs(a));
+ }
+else
+Console.WriteLine("извините, но это не трехзначное число, мы так не работаем! ");
